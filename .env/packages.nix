e00c1f7a54b297104eac40bdb9cc5c05eb422d87
Your Name <you@example.com>
2023-10-29 13:03:48 +0100

    refactor shell.nix

---
+++ .env/packages.nix
@@ -0,0 +1,13 @@
+let
+    fixed_pkgs = import ./pinned.nix;
+in
+[
+    fixed_pkgs.coreutils
+    fixed_pkgs.bash
+
+    fixed_pkgs.ansible_2_13
+    fixed_pkgs.sshpass
+
+    fixed_pkgs.pre-commit
+    fixed_pkgs.envsubst
+]
