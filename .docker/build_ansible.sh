e4a58212d46c47aa83e638a2fcf6e78e3385ec16
Your Name <you@example.com>
2023-10-29 19:21:20 +0100

    update Dockerfile_ansible to use packages.nix

--- .docker/build_ansible.sh
+++ .docker/build_ansible.sh
@@ -2,4 +2,6 @@
 SCRIPT_DIR=${0%/*}
 
 # build
-docker build -f "$SCRIPT_DIR/Dockerfile_ansible" -t mm0n0/ansible_docker:v1 "$SCRIPT_DIR"
+# run build in "$SCRIPT_DIR/.." so .env is in the build context too
+# and can be copied to the docker image
+docker build -f "$SCRIPT_DIR/Dockerfile_ansible" --no-cache -t mm0n0/ansible_docker:v1 "$SCRIPT_DIR/.."
