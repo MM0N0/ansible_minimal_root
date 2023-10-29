d5a93eeaa1c310908c58b608124408bcb95a80a8
Your Name <you@example.com>
2023-10-29 21:08:02 +0100

    update build_ansible.sh

--- .docker/build_ansible.sh
+++ .docker/build_ansible.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=${0%/*}
 # build
 # run build in "$SCRIPT_DIR/.." so .env is in the build context too
 # and can be copied to the docker image
-docker build -f "$SCRIPT_DIR/Dockerfile_ansible" --no-cache -t mm0n0/ansible_docker:v1 "$SCRIPT_DIR/.."
+docker build -f "$SCRIPT_DIR/Dockerfile_ansible" -t mm0n0/ansible_docker:v1 "$SCRIPT_DIR/.."
