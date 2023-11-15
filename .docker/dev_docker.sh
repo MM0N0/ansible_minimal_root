f4747a082d34cb83db4a784805fbad265a6a6efc
Your Name <you@example.com>
2023-11-15 16:45:59 +0100

    update docker setup

--- .docker/dev_docker.sh
+++ .docker/dev_docker.sh
@@ -8,9 +8,11 @@ if [ "$NO_TTY" == "1" ]; then
 fi
 
 docker run --rm$INTERACTIVE_ARG --net="host" --name=ansible_docker \
-  -v "${PWD}":/work:rw \
-  -v /mnt/ramdisk/:/mnt/ramdisk/:ro \
+  -v "${PWD}":/repo:rw \
   -v ~/.ansible:/root/.ansible \
-  mm0n0/ansible_docker:v1 \
+  "mm0n0/ansible_docker:v1" \
   \
-  bash -c "cd /work && $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18}"
+  bash -c "
+    mkdir -p /tmp &&
+    cd /repo &&
+    $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18}"
--- .test/tomcat_apache2/test.sh
+++ .test/tomcat_apache2/test.sh
@@ -25,23 +25,23 @@ docker compose -f "$SCRIPT_DIR/docker-compose.yml" up -d
 
 echo "do root-tasks"
 ROOT_TASKS_CMD="ansible-playbook -i '$SCRIPT_DIR/inventory.yml' '$SCRIPT_DIR/test.yml' --tags all_roles,root_tasks $2"
-"$PROJECT_DIR/.docker/ansible_docker.sh" "$ROOT_TASKS_CMD"
+"$PROJECT_DIR/.docker/dev_docker.sh" "$ROOT_TASKS_CMD"
 
 if [ "$ANSIBLE_TEST_REMOVE_ROOT" == 1 ]; then
   echo "remove root"
   REMOVE_ROOT_CMD="ansible-playbook -i '$SCRIPT_DIR/inventory.yml' '$PROJECT_DIR/.test/remove_sudo_permissions.yml' -e 'set_hosts=all,!localhost' $2"
-  "$PROJECT_DIR/.docker/ansible_docker.sh" "$REMOVE_ROOT_CMD"
+  "$PROJECT_DIR/.docker/dev_docker.sh" "$REMOVE_ROOT_CMD"
 else
   echo "skipped remove root"
 fi
 
 echo "do non-root-tasks"
 NON_ROOT_TASKS_CMD="ansible-playbook -i '$SCRIPT_DIR/inventory.yml' '$SCRIPT_DIR/test.yml' --tags all_roles,non_root_tasks $2"
-"$PROJECT_DIR/.docker/ansible_docker.sh" "$NON_ROOT_TASKS_CMD"
+"$PROJECT_DIR/.docker/dev_docker.sh" "$NON_ROOT_TASKS_CMD"
 
 echo "do test-tasks"
 TEST_TASKS_CMD="ansible-playbook -i '$SCRIPT_DIR/inventory.yml' -e 'ansible_user=root ansible_password=root' '$SCRIPT_DIR/test.yml' $2"
-"$PROJECT_DIR/.docker/ansible_docker.sh" "$TEST_TASKS_CMD"
+"$PROJECT_DIR/.docker/dev_docker.sh" "$TEST_TASKS_CMD"
 
 
 
