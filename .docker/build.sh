d3fd308ec22258dee2061d5b191f1a55d146adc1
Your Name <you@example.com>
2023-11-16 21:41:00 +0100

    update comments

--- .docker/build.sh
+++ .docker/build.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=${0%/*}
 
 # build
 # run build in "$SCRIPT_DIR/.." so .env is in the build context too
-# and can be copied to the docker image
+# and the repository can be copied to the docker image
 docker build -f "$SCRIPT_DIR/Dockerfile" -t mm0n0/dev_docker_ansible_minimal_root:v1 "$SCRIPT_DIR/.."
 docker build -f "$SCRIPT_DIR/Dockerfile_test_host" -t mm0n0/test_host_ansible_minimal_root:v1 "$SCRIPT_DIR"
