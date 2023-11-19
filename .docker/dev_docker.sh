9ac992af86ee83e79ad4b0ecd26be4439a49e274
Your Name <you@example.com>
2023-11-19 12:24:44 +0100

    update dev_docker setup

--- .docker/dev_docker.sh
+++ .docker/dev_docker.sh
@@ -7,19 +7,9 @@ if [ "$NO_TTY" == "1" ]; then
   INTERACTIVE_ARG=""
 fi
 
-if [ -z "$SSH_KEY_FILE" ]; then
-  SSH_KEY_FILE=""
-fi
-
 docker run --rm$INTERACTIVE_ARG --net="host" --name=dev_docker_ansible_minimal_root \
   -v "${PWD}":/repo:rw \
-  -v ~/.ansible:/root/.ansible \
-  --mount type=tmpfs,destination=/ramdisk \
   "mm0n0/dev_docker_ansible_minimal_root:v1" \
   \
   bash -c \
-    "cd /repo && \
-
-    SSH_KEY_FILE=$SSH_KEY_FILE \
-    ANSIBLE_VAULT_PASS_user_vault=$(cat "$ANSIBLE_VAULT_PASS_FILE_user_vault" 2> /dev/null) \
-    bash .docker/inject_credentials_then_run.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18}"
+    "cd /repo && $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18}"
