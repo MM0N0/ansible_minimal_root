47f979b2e9f694a30adf5ca99f41019845b8ae0b
Your Name <you@example.com>
2023-12-01 15:28:37 +0100

    update dev_docker setup

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=${0%/*}
+source "$SCRIPT_DIR/.env"
 
 # build
 # run build in "$SCRIPT_DIR/.." so .env is in the build context too
 # and the repository can be copied to the docker image
-docker build -f "$SCRIPT_DIR/Dockerfile" -t mm0n0/dev_docker_ansible_minimal_root:v1 "$SCRIPT_DIR/.."
-docker build -f "$SCRIPT_DIR/Dockerfile_test_host" -t mm0n0/test_host_ansible_minimal_root:v1 "$SCRIPT_DIR"
+docker build -f "$SCRIPT_DIR/Dockerfile" -t "mm0n0/dev_docker_$PROJECT_NAME:v1" "$SCRIPT_DIR/.."
+docker build -f "$SCRIPT_DIR/Dockerfile_test_host" -t "mm0n0/test_host_$PROJECT_NAME:v1" "$SCRIPT_DIR"
--- .docker/dev_docker.sh
+++ .docker/dev_docker.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+SCRIPT_DIR=${0%/*}
+source "$SCRIPT_DIR/.env"
+
 if [ -z "$NO_TTY" ]; then
   NO_TTY="0"
 fi
@@ -7,8 +10,8 @@ if [ "$NO_TTY" == "1" ]; then
   INTERACTIVE_ARG=""
 fi
 
-docker run --rm$INTERACTIVE_ARG --net="host" --name=dev_docker_ansible_minimal_root \
+docker run --rm$INTERACTIVE_ARG --net="host" --name="dev_docker_$PROJECT_NAME" \
   -v "${PWD}":/repo:rw \
-  "mm0n0/dev_docker_ansible_minimal_root:v1" \
+  "mm0n0/dev_docker_$PROJECT_NAME:v1" \
   \
   bash -c "cd /repo && $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18}"
